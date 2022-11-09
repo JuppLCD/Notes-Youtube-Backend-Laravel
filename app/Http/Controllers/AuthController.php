1794bce58b8fcab90a9bfb220573ec2c7d0ac25e
JuppLCD <lorenzocanovas207@gmail.com>
2022-11-09 19:12:02 -0300

    Create AuthController to handle session by api

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email',
+            'password' => 'required|string|min:5',
+            'confirmPassword' => 'required|same:password'
+        ]);
+
+        $user = User::create($request->only('name', 'password', 'email'));
+
+        if (!$user) {
+            abort(500, 'Error to create user');
+        }
+
+        $res = [
+            'accessToken' => $user->createToken('example')->plainTextToken,
+            'userName' => $user->name
+        ];
+
+        return response()->json($res);
+    }
+
+    public function login(Request $request)
+    {
+        // Test User
+        // 'email' => 'test@example.com', 'password' => 'password'
+
+        $validateData = $request->validate([
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $user = User::where('email',  $validateData['email'])->first();
+
+        if (!($user && Hash::check($validateData['password'], $user->password))) {
+            abort(401, 'Invalid credentials');
+        }
+
+        $res = [
+            'accessToken' => $user->createToken('example')->plainTextToken,
+            'userName' => $user->name
+        ];
+
+        return response()->json($res);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
