7a787e6504f40d6f809a02a7e6f053beeab582cc
JuppLCD <lorenzocanovas207@gmail.com>
2022-11-14 23:30:47 -0300

    Create seeds to Note and NoteList

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
+
+use App\Models\User;
+use App\Models\Note;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +19,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        \App\Models\User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $notes = Note::factory(7)->create([
+            'user_id' => $user->id,
+        ]);
+
+        $this->call(NoteListSeeder::class);
+
+        foreach ($notes as $note) {
+            $note->lists()->attach(rand(1, 3));
+        }
     }
 }
---
+++ database/seeders/NoteListSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\NoteList;
+
+class NoteListSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        NoteList::factory()->create([
+            'title' => 'All Notes (default)',
+        ]);
+
+        NoteList::factory()->create([
+            'title' => 'Programming',
+        ]);
+
+        NoteList::factory()->create([
+            'title' => 'Maths',
+        ]);
+    }
+}
